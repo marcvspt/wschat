80ddac199ae5187e9b6a042763a721dcd04789de
marcvspt <78035694+marcvspt@users.noreply.github.com>
2023-07-29 23:05:54 -0600

    Update project

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
+const express = require('express')
+const http = require('http')
+const socketIO = require('socket.io')
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
+const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
 
-const port = process.env.BACKEND_PORT || 3000;
+const port = process.env.BACKEND_PORT || 3000
 
 io.on('connection', (socket) => {
-    console.log('New client connected');
+    console.log('New client connected')
 
     socket.on('message', (data) => {
-        console.log('Message received:', data);
-        io.emit('message', data);
-    });
+        console.log('Message received:', data)
+        io.emit('message', data)
+    })
 
     socket.on('disconnect', () => {
-        console.log('Client disconnected');
-    });
-});
+        console.log('Client disconnected')
+    })
+})
 
 server.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-});
+    console.log(`Server running on http://localhost:${port}`)
+})
--- frontend/index.js
+++ frontend/index.js
@@ -1,23 +1,23 @@
-const wsserver = "http://ws.example-domain.local" //PUT YOUR SERVER
-const socket = io(wsserver);
+const wsserver = 'http://ws.example-domain.local' //PUT YOUR SERVER-DOMAIN
+const socket = io(wsserver)
 
-const messages = document.getElementById('messages');
-const messageForm = document.getElementById('message-form');
-const inputMessage = document.getElementById('input-message');
+const messages = document.getElementById('messages')
+const messageForm = document.getElementById('message-form')
+const inputMessage = document.getElementById('input-message')
 
 messageForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const message = inputMessage.value;
+    event.preventDefault()
+    const message = inputMessage.value
 
     if (message.trim() !== '') {
-        socket.emit('message', message);
-        inputMessage.value = '';
+        socket.emit('message', message)
+        inputMessage.value = ''
     }
-});
+})
 
 socket.on('message', (data) => {
-    const li = document.createElement('li');
-    li.textContent = data;
+    const li = document.createElement('li')
+    li.textContent = data
 
-    messages.appendChild(li);
-});
+    messages.appendChild(li)
+})
